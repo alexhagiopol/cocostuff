bf08d2435a77836ddb5897090062e4c5bb9e2e56
Alex Hagiopol <alexhagiopol@gmail.com>
2017-10-25 13:04:24 -0700

    update configuration script with symlinks, folder structure, and conversion script

--- configure_deeplab_ubuntu16.sh
+++ configure_deeplab_ubuntu16.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -e
-# download dataset
-wget --directory-prefix=downloads http://calvin.inf.ed.ac.uk/wp-content/uploads/data/cocostuffdataset/cocostuff-10k-v1.1.zip
-unzip downloads/cocostuff-10k-v1.1.zip -d dataset/
-# update submodule and go into correct directory
-git submodule update --init models/deeplab/deeplab-public-ver2
-cd models/deeplab/deeplab-public-ver2
-cp Makefile.config.example Makefile.config
 # install dependencies for Ubuntu 16.04 and Pascal GPUs
 # nvidia driver
 sudo add-apt-repository ppa:xorg-edgers/ppa
@@ -35,6 +28,21 @@ sudo apt-get install -y libatlas-base-dev
 sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
 sudo apt-get install -y libmatio-dev
 sudo apt-get install -y python-numpy
+# download dataset
+wget --directory-prefix=downloads http://calvin.inf.ed.ac.uk/wp-content/uploads/data/cocostuffdataset/cocostuff-10k-v1.1.zip
+unzip downloads/cocostuff-10k-v1.1.zip -d dataset/
+# update submodule
+git submodule update --init models/deeplab/deeplab-public-ver2
+# make folders
+mkdir models/deeplab/deeplab-public-ver2/cocostuff && mkdir models/deeplab/deeplab-public-ver2/cocostuff/data
+# make symlinks to data
+cd models/deeplab/cocostuff/data && ln -s ../../../../dataset/images images && cd ../../../..
+# run anootation conversion script
+python dataset/code/conversion/convertAnnotationsDeeplab.py
+# go into deeplab dir
+cd models/deeplab/deeplab-public-ver2
+# copy makefile config example to actual makefile config
+cp Makefile.config.example Makefile.config
 # compile
 make all -j$(nproc)
 # run tests to make sure your GPU and software work together
